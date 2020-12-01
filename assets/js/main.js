0d58c3ce5ad2f8808f785adaf9b8fd2b8e3856a2
auxfuse <aux.fuse@yahoo.com>
2020-12-01 16:20:49 +0000

    Condensed main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,13 @@
 /* Bowser Settings */
 const browser = bowser.getParser(window.navigator.userAgent);
 console.log(`The current browser name is "${browser.getBrowserName()}" & the version is "${browser.getBrowserVersion()}"`);
+console.log(browser)
 
-const fm = {
-    'safari': '>=12',
+const platform = browser.satisfies({
+    'safari': '>=13',
     'chrome': '>=80',
-    // Old Edge HTML 18.17763 = Edge 44
-    'edge': '>=44',
-    // New Edge
-    'microsoft edge': '>=80',
-    'internet explorer': '>=11'
-};
-
-const platform = browser.satisfies(fm);
+    'microsoft edge': '>=80'
+});
 
 if (!platform) {
     // Hide form
